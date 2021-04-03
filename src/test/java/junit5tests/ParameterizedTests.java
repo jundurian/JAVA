a8f0cfd86e5491d8cbd698d00f41f061264bd87f
Jundurian <gabrieljundurian@gmail.com>
2021-04-02 22:14:40 -0300

    @ParameterizedTest - @ValueSource

---
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -0,0 +1,23 @@
+package junit5tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParameterizedTests {
+
+    @ParameterizedTest(name = "Run: {index} - value: {arguments} ")
+    @ValueSource(ints = {1,5,6})
+    void intValues(int theParam){
+        System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {"firstString", "secondString"})
+    void stringValues(String theParam){
+        System.out.println("theParam = " + theParam);
+    }
+}
