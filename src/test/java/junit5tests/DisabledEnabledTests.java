7f674ea742795ff843624716ea521b06f2dd71d6
Jundurian <gabrieljundurian@gmail.com>
2021-04-03 13:25:30 -0300

    DisabledTests

---
+++ src/test/java/junit5tests/DisabledEnabledTests.java
@@ -0,0 +1,46 @@
+package junit5tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class DisabledEnabledTests {
+
+    @Test
+    @Disabled(value = "Disabled for demo")
+    void firstTest(){
+        System.out.println("First Test");
+    }
+
+    @Test
+    @DisabledOnOs(value = OS.WINDOWS, disabledReason = "Disabled on Windows")
+    //@EnabledOnOs(value = OS.MAC)
+    void secondTest(){
+        System.out.println("Second Test");
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "env", matches = "staging", disabledReason = "Disable if " +
+            "Env is Staging")
+    void thirdTest(){
+        System.out.println("Third Test");
+    }
+
+    @Test
+    @DisabledIf(value = "provider", disabledReason = "Disable")
+    void fourthTest(){
+        System.out.println("Fourth Test");
+    }
+
+    @Test
+    void fifthTest(){
+        System.out.println("Fifth Test");
+    }
+
+    boolean provider() {
+    return LocalDateTime.now().getDayOfWeek().equals(DayOfWeek.SATURDAY);
+    }
+}
